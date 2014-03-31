5fbd582f8916badc13e89f291ef9c565da7a805d
Aidan Zealley <aidan@zealley.com>
2014-03-31 20:32:36 +0100

    added photo lightbox and google analytics

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
-	$('#mywork').click(function(e){ 
+	$('#mywork').click(function(e){
 		e.preventDefault();
-		linkLocation = this.href;  
+		linkLocation = this.href;
 
 		$('.intro').removeClass('bounceInLeft');
 		$('.intro').addClass('bounceOutRight');
@@ -10,13 +10,30 @@ $(document).ready(function() {
 		setTimeout(function(){window.location = linkLocation}, 400);
 	});
 
-	$('#webwork a').click(function(e){ 
+	$('#webwork a').click(function(e){
 		e.preventDefault();
-		linkLocation = this.href;  
+		linkLocation = this.href;
 
 		$(this).find('img').addClass('animated bounceOutUp');
  
 		setTimeout(function(){window.location = linkLocation}, 400);
 	});
+
+	$('#photowork .animated-link').click(function(){
+		var image = $(this).attr('href');
+
+		$('body').append('<div class="modal-overlay animated bounceInUp"></div>');
+		$('.modal-overlay').append('<button id="close-modal">&#10060;</button>');
+		$('.modal-overlay').append('<div class="modal"><div class="spinner"></div>');
+		$('.modal').append('<img src="' + image + '" alt="hi"/>');
+
+		return false;
+	});
+
+	$('body').on('click', '#close-modal', function() {
+		$('.modal-overlay').removeClass('bounceInUp');
+		$('.modal-overlay').addClass('bounceOutUp');
+		setTimeout(function(){$('.modal-overlay').remove();}, 800);
+    });
 
 });
