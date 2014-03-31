b9bbf823890c235e6ebaacfa65aba75192fd20dc
Aidan Zealley <aidan@zealley.com>
2014-03-31 16:30:02 +0100

    New animations, new photos section

--- js/main.js
+++ js/main.js
@@ -7,7 +7,16 @@ $(document).ready(function() {
 		$('.intro').removeClass('bounceInLeft');
 		$('.intro').addClass('bounceOutRight');
  
-		setTimeout(function(){window.location = linkLocation}, 800);
+		setTimeout(function(){window.location = linkLocation}, 400);
+	});
+
+	$('#webwork a').click(function(e){ 
+		e.preventDefault();
+		linkLocation = this.href;  
+
+		$(this).find('img').addClass('animated bounceOutUp');
+ 
+		setTimeout(function(){window.location = linkLocation}, 400);
 	});
 
 });
